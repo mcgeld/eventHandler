4b7f17ea10ccb152d9d3d738a327fe9feb62e49c
thundercloud90 <wade.w.wilkey@gmail.com>
2015-05-04 23:21:09 -0600

    Initial Commit

---
+++ EventHandler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventHandler
+//
+//  Created by Wade Wilkey on 4/5/2015.
+//  Copyright (c) 2015 Wade Wilkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
