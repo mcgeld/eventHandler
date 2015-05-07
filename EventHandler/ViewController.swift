c90b0f1bdd262b56664acc340e6ed8bfea82a386
Brittny Wright <brittnywright@Brittnys-MacBook-Air.local>
2015-05-07 17:19:24 -0600

    Map updates location, and also gives description of location.

--- EventHandler/ViewController.swift
+++ EventHandler/ViewController.swift
@@ -15,10 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
     //Segmented View
@@ -41,6 +38,13 @@ class ViewController: UIViewController {
         }
         
         
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 }
