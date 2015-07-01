b3d309e94448ef8f8544ee84220a8b3649a30f77
Brittny Wright <brittnywright@Brittnys-MacBook-Air.local>
2015-06-30 22:42:02 -0600

    minor

--- EventHandler/friendContainerView.swift
+++ EventHandler/friendContainerView.swift
@@ -10,81 +10,71 @@ import Foundation
 import UIKit
 import AddressBook
 
-class friendContainerView: UITableViewController, UITableViewDataSource, UITableViewDelegate, ABAddressBookRef
+class friendContainerView: UITableViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate
 {
  
+    @IBOutlet var searchBar: UISearchBar!
     
-    var adbk : ABAddressBook!
+   
     var friends : [User] = []
     
     override func viewDidLoad()
     {
-        var status=determineStatus();
-        getContactNames();
+       
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
         
     }
+    func DismissKeyboard()
+    {
+        view.endEditing(true)
+    }
     
-    func createAddressBook() -> Bool {
-        if self.adbk != nil {
-            return true
-        }
-        var err : Unmanaged<CFError>? = nil
-        let adbk : ABAddressBook? = ABAddressBookCreateWithOptions(nil, &err).takeRetainedValue()
-        if adbk == nil {
-            println(err)
-            self.adbk = nil
-            return false
-        }
-        self.adbk = adbk
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        resignFirstResponder()
         return true
     }
     
-    func determineStatus() -> Bool {
-        let status = ABAddressBookGetAuthorizationStatus()
-        switch status {
-        case .Authorized:
-            return self.createAddressBook()
-        case .NotDetermined:
-            var ok = false
-            ABAddressBookRequestAccessWithCompletion(nil) {
-                (granted:Bool, err:CFError!) in
-                dispatch_async(dispatch_get_main_queue()) {
-                    if granted {
-                        ok = self.createAddressBook()
-                    }
-                }
-            }
-            if ok == true {
-                return true
-            }
-            self.adbk = nil
-            return false
-        case .Restricted:
-            self.adbk = nil
-            return false
-        case .Denied:
-            self.adbk = nil
-            return false
-        }
+    
+    
+    // MARK: - Table view data source
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+       
+        self.tableView.reloadData()
     }
     
-    func getContactNames() {
-        if !self.determineStatus() {
-            println("not authorized")
-            return
-        }
-        let people = ABAddressBookCopyArrayOfAllPeople(adbk).takeRetainedValue() as NSArray as [ABRecord]
-            
-        
-        for person in people
-        {
-          
-            
-            println(ABRecordCopyCompositeName(person).takeRetainedValue())
-            
-            
-        
-        }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        
+        return friends.count
+        
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Configure the cell...
+        let i = indexPath.row
+        cell.textLabel!.text = friends[indexPath.row].firstName + " " + friends[indexPath.row].lastName
+        
+        return cell
+    }
+    
+
+
+  
+
     
 }
